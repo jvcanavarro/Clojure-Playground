0e32e8bc4cf7b0fb3bd3b7e5ef13281b2248b830
jvcanavarro <jvcanavarro@gmail.com>
2019-05-03 18:34:50 -0300

    Added 16 Answers

--- answers_4clojure.clj
+++ answers_4clojure.clj
@@ -39,3 +39,47 @@
 (= #{:c :b :d :a} (set '(:a :a :b :c :c :c :c :d :d)))
 
 (= #{:c :b :d :a} (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+;; 09: Sets: conj
+
+(= #{1 2 3 4} (conj #{1 4 3} 2))
+
+;; 10: Intro to Maps
+
+(= 20 ((hash-map :a 10, :b 20, :c 30) :b))
+
+(= 20 (:b {:a 10, :b 20, :c 30}))
+
+;; 11: Maps: conj
+
+(= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))
+
+;; 12: Intro to Sequences
+
+(= 3 (first '(3 2 1)))
+
+(= 3 (second [2 3 4]))
+	
+(= 3 (last (list 1 2 3)))
+
+;; 13: Sequences: rest
+ 
+(= [20 30 40] (rest [10 20 30 40])
+
+;; 14: Intro to Functions
+
+(= 8 ((fn add-five [x] (+ x 5)) 3))
+
+(= 8 ((fn [x] (+ x 5)) 3))
+
+(= 8 (#(+ % 5) 3))
+
+(= 8 ((partial + 5) 3))
+
+;; 15: Double Down
+
+(= (* 2 2) 4)
+
+;; 16: Hello World
+
+(= (#(str "Hello, " % "!")"Dave") "Hello, Dave!")
